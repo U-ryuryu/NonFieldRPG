d249df3d46f24b3d2a487d1eca15cc9b59a761e2
U-ryuryu <kugyuryuryu@gmail.com>
2021-06-07 16:15:25 +0900

    Enemyに遭遇する実装

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     public StageUIManager stageUI;
+    // 敵に遭遇するテーブル：-1なら遭遇しない、0なら遭遇
+    int[] encountTable = {-1, -1, 0, -1, 0, -1};
     int currentStage = 0;  //現在のステージ進行度
 
     private void Start()
@@ -15,8 +17,12 @@ public class QuestManager : MonoBehaviour
     public void OnNextButton()
     {
         currentStage++;
-        Debug.Log("進行度増加"+ currentStage);
         // 進行度をUIに反映
         stageUI.UpdateUI(currentStage);
+        
+        if (encountTable[currentStage] == 0 )
+        {
+            Debug.Log("敵に遭遇");
+        }
     }
 }

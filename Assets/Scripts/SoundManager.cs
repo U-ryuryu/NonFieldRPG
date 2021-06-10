3b18d005a26cd326ae266a7604a98be8fe6f453a
U-ryuryu <kugyuryuryu@gmail.com>
2021-06-10 09:39:13 +0900

    シングルトンを用いたBGMの実装

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -19,11 +19,31 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    public AudioSource audioSourceBGM;
+    public AudioClip[] audioClipsBGM;
     public AudioSource audioSourceSE;
     public AudioClip audioClip;
 
-    void Start()
+    public void PlayBGM(string sceneName)
     {
+        audioSourceBGM.Stop();
+        switch (sceneName)
+        {
+            default:
+            case "Title":
+                audioSourceBGM.clip = audioClipsBGM[0];
+                break;
+            case "Town":
+                audioSourceBGM.clip = audioClipsBGM[1];
+                break;
+            case "Quest":
+                audioSourceBGM.clip = audioClipsBGM[2];
+                break;
+            case "Battle":
+                audioSourceBGM.clip = audioClipsBGM[3];
+                break;
+        }
+        audioSourceBGM.Play();
     }
 
     public void PlaySE()
